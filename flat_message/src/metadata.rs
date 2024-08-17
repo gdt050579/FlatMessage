08dd95a5d5995b164bb82f1b046f8f6432f31b55
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-08-17 10:17:23 +0300

    Self reference bug fix

--- flat_message/src/metadata.rs
+++ flat_message/src/metadata.rs
@@ -46,26 +46,26 @@ impl MetaDataBuilder {
     pub fn build(self) -> MetaData {
         self.metadata
     }
-    pub fn timestamp(&mut self, timestamp: u64) -> &mut Self {
+    pub fn timestamp(mut self, timestamp: u64) -> Self {
         self.metadata.timestamp = NonZeroU64::new(timestamp);
         self
     }
-    pub fn now(&mut self) -> &mut Self {
+    pub fn now(mut self) -> Self {
         match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
             Ok(d) => self.metadata.timestamp = NonZeroU64::new(d.as_millis() as u64),
             Err(_) => self.metadata.timestamp = None,
         }
         self
     }
-    pub fn unique_id(&mut self, unique_id: u64) -> &mut Self {
+    pub fn unique_id(mut self, unique_id: u64) -> Self {
         self.metadata.unique_id = NonZeroU64::new(unique_id);
         self
     }
-    pub fn auto_unique_id(&mut self) -> &mut Self {
+    pub fn auto_unique_id(mut self) -> Self {
         self.metadata.unique_id = NonZeroU64::new(GLOBAL_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed));
         self
     }
-    pub fn version(&mut self, version: u8) -> &mut Self {
+    pub fn version(mut self, version: u8) -> Self {
         self.metadata.version = NonZeroU8::new(version);
         self
     }
