caec800e05f06990ae60eb6a1b5d4e3e7fda4b3f
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2025-09-12 09:28:54 +0300

    variant results for windows

---
+++ book/chapter-5/extract_overall_stats.py
@@ -0,0 +1,53 @@
+import os
+
+global_speed = {}
+def process_file(file_path):
+    global global_speed
+    exec_name = ""
+    in_table = False
+    data_size = 0
+    n = 0
+    for line in open(file_path,"rt", encoding="utf-8"):
+        if line.startswith("### 1. Windows Execution"):
+            exec_name = "win"
+        elif line.startswith("### 2. MacOs Execution"):
+            exec_name = "mac"
+        elif line.startswith("### 3. Linux Execution"):
+            exec_name = "linux"
+        if line.startswith("* Data size: "):
+            data_size = int(line.strip().replace("* Data size: ","").replace("bytes","").replace("`","").strip())
+        if line.startswith("* Serialization"):
+            n = int(line.split("`n = ",1)[1].replace("`","").strip())
+
+
+        if line.startswith("| Algorithm |"):
+            in_table = True
+            continue
+        if line.strip() == "":
+            in_table = False
+            continue
+        if not in_table:
+            continue
+        if line.startswith("|"):
+            w = line.strip().split("|")
+            des_name = w[1].strip()
+            name = des_name.split(" ",1)[0].strip()
+            if name.startswith("-"): continue
+            time = w[5].strip().split("<",1)[0].replace("*","").strip()
+            ts = data_size * n / 1000000
+            if not time.startswith("-"):
+                st = float(time.strip())
+                speed = ts * 1000.0 / st
+            else:
+                speed = "-"
+            # print(exec_name, name, time, data_size, n, "speed = ",speed, " MB/sec")
+            if not exec_name in global_speed:
+                global_speed[exec_name] = {}
+            if not des_name in global_speed[exec_name]:
+                global_speed[exec_name][des_name] = []
+            global_speed[exec_name][des_name] += [speed]
+
+for name in os.listdir("./results"):
+    process_file(os.path.join("./results", name))
+
+print(global_speed)
